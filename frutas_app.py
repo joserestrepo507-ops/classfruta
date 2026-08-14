8c63d1a2d379e52fdef8ba5e2164f02b139ad6d2
joserestrepo507-ops <jose.restrepo507@pascualbravo.edu.co>
2026-08-14 02:07:31 -0500

    Update frutas_app.py

--- frutas_app.py
+++ frutas_app.py
@@ -77,6 +77,4 @@ st.subheader("Resultado")
 st.success(f"La fruta más parecida es: {fruta_mas_parecida}")
 
 # Mostramos la imagen correspondiente, centrada
-col1, col2, col3 = st.columns([1, 1, 1])
-with col2:
-    st.image(imagenes[fruta_mas_parecida], caption=fruta_mas_parecida, width=250)
+st.image(imagenes[fruta_mas_parecida], caption=fruta_mas_parecida, width=250) 
